4ea7692d6684d6200b83106b3c23454388e4b0eb
Tomasz <tomasz9531@gmail.com>
2021-09-18 18:29:07 +0200

    Create verify commands

---
+++ app/Console/Commands/VerifyEmailCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use DB;
+
+class VerifyEmailCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'email:verify {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Verify user email.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+
+        $updated = DB::table('users')
+            ->where('email', $email)
+            ->update([
+                'email_verified_at' => date('Y-m-d H:i:s')
+            ]);
+
+        if($updated === 0) {
+            return $this->error('User has been not found.');
+        }
+
+        return $this->info($email.' has been verified.');
+    }
+}
---
+++ app/Console/Commands/VerifyUserCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use DB;
+
+class VerifyUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:verify {nick}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Verify user.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $nick = $this->argument('nick');
+
+        $updated = DB::table('users')
+            ->where('nick', $nick)
+            ->update([
+                'verified' => true
+            ]);
+
+        if($updated === 0) {
+            return $this->error('User has been not found.');
+        }
+
+        return $this->info($nick.' has been verified.');
+    }
+}
